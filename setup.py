8b3f03fa2bfca655fee58428272c4a5bf679ee46
Luigi Di Sotto <luigi.disotto@aitechnologies.it>
2021-04-21 16:19:12 +0200

    update requirements for python, now 3.7+

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setuptools.setup(
     ],
     package_dir={"": "src"},
     packages=setuptools.find_packages(where="src"),
-    python_requires=">=3.8",
+    python_requires=">=3.7",
     install_requires=[
        "flatten-dict",
    ],
